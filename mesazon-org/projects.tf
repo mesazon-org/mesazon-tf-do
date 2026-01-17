268527d773ec2eab4b28bd37cd60bea23429b19d
Andreas Rigas <andreas.rigas@outlook.com>
2026-01-17 23:23:42 +0200

    feat: change name

--- mesazon-org/projects.tf
+++ mesazon-org/projects.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_project" "mesazon_dev" {
-  name        = "Mesazon Dev"
+  name        = "mesazon-dev"
   description = "Development environment for Mesazon"
   purpose     = "Testing environment for Mesazon"
   environment = "Development"
