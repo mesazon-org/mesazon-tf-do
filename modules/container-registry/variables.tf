dc12a764a1ebdcada51a26c12e1c1fb434396538
Andreas Rigas <andreas.rigas@outlook.com>
2026-01-19 20:58:14 +0200

    feat: try that

--- modules/container-registry/variables.tf
+++ modules/container-registry/variables.tf
@@ -1,14 +1,11 @@
 variable "registry_name" {
   type     = string
-  required = true
 }
 
 variable "subscription_tier" {
   type     = string
-  required = true
 }
 
 variable "region" {
   type     = string
-  required = true
 }
