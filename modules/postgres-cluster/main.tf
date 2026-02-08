24294b0d5d6836480fe70801957f2c7f0f641d64
Andreas Rigas <andreas.rigas@outlook.com>
2026-02-09 01:16:46 +0200

    feat: Improve networking

--- modules/postgres-cluster/main.tf
+++ modules/postgres-cluster/main.tf
@@ -61,8 +61,8 @@ resource "digitalocean_database_postgresql_config" "pg_config" {
 resource "digitalocean_database_firewall" "block_all" {
   cluster_id = digitalocean_database_cluster.pg_cluster.id
 
-    rule {
-      type  = "tag"
-      value = "block-all"
-    }
+  rule {
+    type  = "ip_addr"
+    value = "127.0.0.1"
+  }
 }
