dd5af200bd0aebeaa60b9544cb67ff698c7587e0
Andreas Rigas <48296471+andrewrigas@users.noreply.github.com>
2026-07-01 22:38:21 +0100

    feat: 313 introduce gateway logos bucket (#12)

---
+++ mesazon-clusters/dev/gateway-spaces.tf
@@ -0,0 +1,9 @@
+module "gateway_organization_logos_bucket" {
+  source = "../../modules/spaces-bucket"
+
+  environment = local.environment
+  region      = local.region
+
+  bucket_name_raw = "gateway-organization-logos"
+  acl             = "public-read"
+}
